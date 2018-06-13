1158ec722ee635d9e28abe5169d6c227fb3edb4d
Lucy Wall <lhwall@gmail.com>
2018-06-13 23:17:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,9 +12,9 @@ module FormsLab
         erb :"pirates/new"
     end
 
-    post "/pirates" do 
-      erb: :"pirates/show"
-    end 
+    post "/pirates" do
+      erb :"pirates/show"
+    end
 
 
 
